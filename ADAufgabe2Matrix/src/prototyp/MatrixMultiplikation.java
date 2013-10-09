25543aecfd9d2c5cf56a1af9347264afecce0d2d
Chrischschi <Christian.Schirin@haw-hamburg.de>
2013-10-09 19:56:19 +0200

    erster commit, muss noch eine menge getan werden 1. fertig implementieren von prototyp 2. interface erstellen 2b. tests 3. Implementation array (trivial) 4. Implementation sparse matrix I und II

---
+++ ADAufgabe2Matrix/src/prototyp/MatrixMultiplikation.java
@@ -0,0 +1,125 @@
+/** Lizensiert unter WTFPL /Licensed under the WTFPL.  http://www.wtfpl.net/*/
+package prototyp;
+
+import java.util.Arrays;
+
+public class MatrixMultiplikation {
+	public static void main(String[] args) {
+		double[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		double[][] matrix2 = {{2,0,3},{1,2,4},{1,0,1}};
+		
+		double[][] result = {{7,4,14},{19,10,38},{31,16,62}};
+		
+		System.out.println("mult");
+		
+		System.out.println(Arrays.deepToString(mult(matrix2,matrix)));
+		System.out.println(Arrays.deepToString(result));
+		
+		System.out.println("add");
+		
+		System.out.println(Arrays.deepToString(add(matrix,matrix2)));
+		System.out.println(Arrays.deepToString(add(matrix2,matrix)));
+		System.out.println(Arrays.deepToString(new double[][]{{3,2,6},{5,7,10},{8,8,10}}));
+		
+		System.out.println("mult scalar"); 
+		
+		System.out.println(Arrays.deepToString(mult(new double[][]{{5,2},{9,12}},2)));
+		System.out.println(Arrays.deepToString(new double[][]{{10,4},{18,24}}));
+		
+		
+		
+		
+	}
+	
+	public static double[][] mult(double[][] a,double[][] b) {
+		if(a.length != a[0].length || b.length != b[0].length) throw new IllegalArgumentException("Eine der beiden Matritzen ist nicht quadratisch");
+		if(a.length != b.length) throw new IllegalArgumentException("Die matritzen haben nicht die gleiche dimension");
+		
+		double[][] c = new double[a.length][b.length]; //ist wayne ob jetzt a.length oder b.length, da a.length == b.length
+
+		int n = a[0].length; //oder b.length 
+		
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < b.length; j++) {
+				for(int k = 0; k < n; k++) {
+					c[i][j] = c[i][j] + a[i][k]*b[k][j];
+				}
+			}
+		}
+		
+		return c;
+		
+	}
+	
+	// Ist diese operation quadratisch oder linear? 
+	public static double[][] add(double[][] a, double[][] b) { 
+		if(a.length != b.length || a[0].length != b[0].length) throw new IllegalArgumentException("Matrix A und B müssen die gleiche anzahl von spalten und zeilen haben");
+		
+		double[][] c = new double[a.length][b[0].length]; 
+		
+		for(int i = 0; i < a.length; i++ ) {
+			for(int j = 0; j < a[0].length; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+			}
+		} 
+		
+		return c;
+		
+	}
+	
+	// Ist diese operation quadratisch oder linear? 
+	public static double[][] mult(double[][] m, double scalar) {
+		double[][] result = new double[m.length][m[0].length];
+		
+		for(int i = 0; i < m.length; i++) {
+			for(int j = 0; j < m[0].length; j++) {
+				result[i][j] = m[i][j] * scalar;
+			}
+		}
+		
+		return result;
+	}
+	/** Exponieren */
+	public static double[][] power(double[][] m, int exponent) {
+		if (exponent > 0 ) 	{return posPow(m,exponent);} // exponent ist größer 0
+		else if (exponent == 0) {return identityMatrix(m);} // exponent ist 0
+		else {return negPow(m,exponent);} // exponent ist negativ
+	}
+	
+	private static double[][] posPow(double[][] m, int exponent) {
+		double[][] accu = new double[m.length][m[0].length];
+		
+		accu = add(accu,m); //initialisierung von accu
+		if(exponent == 1) return accu; 
+		else return mult(accu,posPow(m,exponent-1)); //TODO: endrekursiv machen und letztendlich zu while transformieren
+	}
+	
+	public static double[][] identityMatrix(double[][] m) {
+		if(!istNxnMatrix(m)) throw new IllegalArgumentException("Matrix muss quadratisch sein, um die einheitsmatrix davon bilden zu können");
+		double[][] id = new double[m.length][m[0].length];
+		for(int i = 0; i < m.length; i++) {
+			for(int j = 0; j < m.length; j++) {
+				if (i == j) id[i][j] = 1; //diagonale erwischt => eins schreiben
+				else id[i][j] = 0; //sonst 0
+ 			}
+		}
+		
+		return id;
+	}
+	
+	private static double[][] negPow(double[][] m,int exponent) {
+		//TODO implementieren
+		return null;
+	}
+	/** Prüft ob quadratische matrix oder nicht */
+	public static boolean istNxnMatrix(double[][] m) {
+		//prüfen auf dem ersten und letzten index, ob die innere länge(spaltenlänge) der äußeren länge (zeilenlänge) entspricht
+		return m.length == m[0].length && m.length == m[m.length-1].length; 
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ ADAufgabe2Matrix/src/prototyp/MatrixMultiplikationTest.java
@@ -0,0 +1,44 @@
+/** Lizensiert unter WTFPL /Licensed under the WTFPL.  http://www.wtfpl.net/*/
+package prototyp;
+
+
+import static org.junit.Assert.*;
+import static prototyp.MatrixMultiplikation.*;
+
+import org.junit.Test;
+
+public class MatrixMultiplikationTest {
+	
+	@Test
+	public void testMult3Times() {
+		double[][] m1 = {{1,2},{3,4}};
+		double[][] expected = {{37, 54}, {81, 118}};
+		assertArrayEquals(
+				"{{1,2},{3,4}}*{{1,2},{3,4}}*{{1,2},{3,4}} = {{37, 54}, {81, 118}}",
+				expected, mult(mult(m1,m1),m1)
+				);
+	}
+
+	@Test
+	public void testPower1Pos() {
+		double[][] m1 = {{1,2},{3,4}};
+		double[][] expected = {{37, 54}, {81, 118}};
+		assertArrayEquals("{{1,2},{3,4}}^3 = {{37, 54}, {81, 118}}",expected,power(m1,3));
+      
+	}
+
+	@Test
+	public void testIdentityMatrix2() {
+		double[][] m1 = new double[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		double[][] identity4x4 = new double[][] {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+		assertArrayEquals(MatrixMultiplikation.identityMatrix(m1), identity4x4);
+	}
+	
+	@Test
+	public void testIdentityMatrix() {
+		double[][] m1 = new double[2][2];
+		double[][] identity2x2 = new double[][] {{1,0},{0,1}};
+		assertArrayEquals(MatrixMultiplikation.identityMatrix(m1), identity2x2);
+	}
+
+}
